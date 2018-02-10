e4eaa035533924e2d0a9da9fac300bd9580adfa3
Robert Hostlowsky <robert.hostlowsky@codecentric.de>
2018-02-10 23:31:44 +0100

    5.2 add apollo subscriptions

--- src/App.js
+++ src/App.js
@@ -2,30 +2,60 @@
 import React, { Component } from 'react';
 
 import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-
-import { createHttpLink } from 'apollo-link-http';
-
 import { ApolloProvider } from 'react-apollo';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+/* http link */
+import { createHttpLink } from 'apollo-link-http';
+/* ws link */
+import { WebSocketLink } from 'apollo-link-ws';
+
+import { getMainDefinition } from 'apollo-utilities';
+import { split } from 'apollo-link';
+/**/
 
 import './App.css';
-
 import { CoolBoard } from './components/CoolBoard';
 
-function createClient() {
-  return new ApolloClient({
-    link: createHttpLink({
-      uri: 'http://localhost:4466/CoolBoardDB/dev',
-    }),
-    cache: new InMemoryCache(),
-  });
-}
+// Create a Http link
+let httpLink = createHttpLink({
+  uri: 'http://localhost:4466/CoolBoardDB/dev',
+});
+
+// Create a WebSocket link:
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4466/CoolBoardDB/dev`,
+  options: {
+    reconnect: true,
+  },
+});
+
+// using the ability to split links, you can send data to each link
+// depending on what kind of operation is being sent
+const returnTrueIfSubscription = ({ query }) => {
+  const { kind, operation } = getMainDefinition(query);
+  return (
+    kind === 'OperationDefinition' &&
+    operation === 'subscription'
+  );
+};
+
+// split based on operation type
+const link = split(
+  returnTrueIfSubscription,
+  wsLink,
+  httpLink
+);
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+});
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ApolloProvider client={createClient()}>
+        <ApolloProvider client={client}>
           <CoolBoard boardId="cjd90t1gw0019018143trudyk" />
         </ApolloProvider>
       </div>
