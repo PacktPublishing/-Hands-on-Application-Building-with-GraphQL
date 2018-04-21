e50ae20a8272a5ebdaea97d84521c6a57a39b188
Robert Hostlowsky <robert.hostlowsky@codecentric.de>
2018-04-22 01:58:39 +0200

    3.3 Integrate Apollo Framework

--- src/App.js
+++ src/App.js
@@ -1,37 +1,87 @@
 /* eslint-disable react/prop-types,react/jsx-key */
 import React, { Component } from 'react';
+
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+import { SchemaLink } from 'apollo-link-schema';
+
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import { ApolloProvider } from 'react-apollo';
+
+import { schema } from './schema';
+
 import './App.css';
 
-import { BoardContainer, List } from './components';
+import { BoardContainer } from './components';
+import { CardList } from './components/CardList';
+// import { Card } from './components/Card';
 
-import boardData from './dummyData';
-
-const renderLists = (lists = []) => {
-  return lists.map(list => (
-    <List
-      name={list.name}
-      cards={list.cards}
-      key={list.id || list.name}
-    />
-  ));
+const Board = ({ board }) => {
+  const { name, lists = [] } = board;
+  return (
+    <BoardContainer boardName={name}>
+      {lists.map(list => <CardList cards={list.cards} name={list.name}/>)}
+    </BoardContainer>
+  );
 };
 
-class Board extends Component {
-  render() {
-    const { data = {} } = this.props;
-    const { name, lists } = data;
+const BoardAdapter = ({ data }) => {
+  const { loading, error, board } = data;
+
+  if (loading) {
+    return <div>Loading Board</div>;
+  }
+  if (error) {
     return (
-      <BoardContainer boardName={name}>
-        {renderLists(lists)}
-      </BoardContainer>
+      <h2>
+        sorry, some error... <span>{error}</span>
+      </h2>
     );
   }
+  if (board) {
+    return <Board board={board} />;
+  }
+
+  return <div>Board does not exist.</div>;
+};
+
+const BoardQuery = gql`
+  query {
+    board: Board(id: "cjc10rgoj721s0147ui3wzapk") {
+      name
+      lists {
+        name
+        cards {
+          name
+          id
+        }
+      }
+    }
+  }
+`;
+/*
+...CardList_list
+${CardList.fragments.list}
+*/
+
+const CoolBoard = graphql(BoardQuery)(BoardAdapter);
+
+function createClient() {
+  return new ApolloClient({
+    link: new SchemaLink({ schema }),
+    cache: new InMemoryCache(),
+  });
 }
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Board data={boardData} />
+        <ApolloProvider client={createClient()}>
+          <CoolBoard />
+        </ApolloProvider>
       </div>
     );
   }
---
+++ src/components/Card.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import gql from 'graphql-tag';
+
+export const CardComponent = styled.div`
+  border-radius: 3px;
+  margin: 0.1em 0 0 0;
+  border-bottom: 1px solid #ccc;
+  background-color: #fff;
+  padding: 10px;
+`;
+
+
+export const Card = ({ name }) => <CardComponent>{name}</CardComponent>;
+
+Card.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+
+Card.fragments = {
+  card: gql`
+    fragment Card_card on Card {
+      name
+    }
+  `,
+};
